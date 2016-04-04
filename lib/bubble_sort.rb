6faaa5dca0fe9e69ee1c742e8f8f41cd60abfd46
Ryan Batty <rgbatty@outlook.com>
2016-04-03 18:48:29 -0600

    First pass at refactoring BubbleSort. BubbleSort is one ugly algorithm

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -2,22 +2,25 @@ class BubbleSort
 
   def sort(array)
     if array.length <= 1
-      return array
-    end
-    swap_counter = 0
-
-    while(swap_counter >= 0)
-
-      (array.length - 1).times do |current|
-        if array[current] > array[current+1]
-          array[current], array[current+1] = array[current+1], array[current]
-          swap_counter += 1
-        else
-          swap_counter -= 1
+      array
+    else
+      swap_counter = 0
+      while(swap_counter >= 0)
+        (array.length - 1).times do |current|
+          if array[current] > array[current+1]
+            swap(array, current, current+1)
+            swap_counter += 1
+          else
+            swap_counter -= 1
+          end
         end
       end
+      array
     end
-    array
+  end
+
+  def swap(array,current, next_value)
+    array[current], array[next_value] = array[next_value], array[current]
   end
 
 end
