9151070df6983b46455ae4a08e0cf73563147b88
Ryan Batty <rgbatty@outlook.com>
2016-04-04 00:18:32 -0600

    completed Benchmark and associated tests.

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -1,12 +1,20 @@
 module SortingSuite
   class Benchmark
 
-    def time(sorting_type, array)
+    def time(sorting_type, array=[3,3,4,5,1])
       sorter = sorting_type.new
       t1 = Time.now.to_f * 1000
       result = sorter.sort(array)
       t2 = Time.now.to_f * 1000
       return_correct_result(sorting_type, t1, t2)
+    end
+
+    def get_time(sorting_type, array)
+      sorter = sorting_type.new
+      t1 = Time.now.to_f * 1000
+      result = sorter.sort(array)
+      t2 = Time.now.to_f * 1000
+      (t2-t1).round(6)
     end
 
     def return_correct_result (sorting_type, t1, t2)
@@ -19,5 +27,32 @@ module SortingSuite
       end
     end
 
+    def fastest(array)
+      bubbleTime = get_time(SortingSuite::BubbleSort, array)
+      insertTime = get_time(SortingSuite::InsertionSort, array)
+      mergeTime = get_time(SortingSuite::MergeSort, array)
+      fastest = [bubbleTime, insertTime, mergeTime].min
+      if fastest == bubbleTime
+        "BubbleSort is the fastest"
+      elsif fastest == insertTime
+        "InsertionSort is the fastest"
+      elsif fastest == mergeTime
+        "MergeSort is the fastest"
+      end
+    end
+
+    def slowest(array)
+      bubbleTime = get_time(SortingSuite::BubbleSort, array)
+      insertTime = get_time(SortingSuite::InsertionSort, array)
+      mergeTime = get_time(SortingSuite::MergeSort, array)
+      slowest = [bubbleTime, insertTime, mergeTime].max
+      if slowest == bubbleTime
+        "BubbleSort is the slowest"
+      elsif slowest == insertTime
+        "InsertionSort is the slowest"
+      elsif slowest == mergeTime
+        "MergeSort is the slowest"
+      end
+    end
   end
 end
--- test/benchmark_test.rb
+++ test/benchmark_test.rb
@@ -21,4 +21,28 @@ class BenchmarkTest < MiniTest::Test
     assert insert
     assert merge
   end
+
+  def test_benchmark_time_no_array
+    benchmark = SortingSuite::Benchmark.new
+
+    merge = benchmark.time(SortingSuite::MergeSort)
+
+    assert merge
+  end
+
+  def test_benchmark_fastest
+    benchmark = SortingSuite::Benchmark.new
+
+    fastest = benchmark.fastest([2, 8, 1, 0, 5])
+
+    assert fastest
+  end
+
+  def test_benchmark_fastest
+    benchmark = SortingSuite::Benchmark.new
+
+    slowest = benchmark.slowest([1, 2, 3, 4, 5])
+    
+    assert slowest
+  end
 end
